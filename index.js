08098f5d996e1c150f2929b61368ef90e004c155
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-09-11 16:23:11 +0530

    ok

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const router1 = jsonServer.router("companyDetails.json");
+const router1 = jsonServer.router("db.json");
+const router = jsonServer.router("companyDetails.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 
 // Create routes for the two JSON files
-server.use("/api", router); // Access data from "db.json" at /api
-server.use("/company", router1); // Access data from "companyDetails.json" at /companies
+server.use("/api", router1); // Access data from "db.json" at /api
+server.use("/company", router); // Access data from "companyDetails.json" at /companies
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
