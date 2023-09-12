3b68e803c6096516403b3b9211b4bb6bfdfa7bd9
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-09-12 12:50:23 +0530

    ok

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
+const express = require("express");
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("companyDetails.json");
-const router2 = jsonServer.router("otherData.json"); 
-const hrAndCompany = jsonServer.router("hr&CompanyCotainer.json");
-const middlewares = jsonServer.defaults();
+
+const app = express();
 const port = process.env.PORT || 8080;
 
-server.use(middlewares);
+// JSON Server Routers
+const router1 = jsonServer.router("companyDetails.json");
+const router2 = jsonServer.router("otherData.json");
+const router3 = jsonServer.router("hr&CompanyContainer.json");
 
-// Use multiple routers
-server.use("/hr&company_container", router); // Use the first JSON file under the /api route
-server.use("/api2", router2); // Use the second JSON file under the /api2 route
-server.use("/hrAndCompany", hrAndCompany); // Use the second JSON file under the /api2 route
+// Enable CORS for both routers
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-server.listen(port, () => {
+// Mount the JSON Server Routers
+app.use("/api", router1);
+app.use("/api2", router2);
+app.use("/hrAndCompany", router3);
+
+app.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+
+// const jsonServer = require("json-server"); // importing json-server library
+// const server = jsonServer.create();
+// const router = jsonServer.router("companyDetails.json");
+// // const router1= jsonServer.router("companyDetails.json")
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+// server.use(middlewares);
+// server.use(router);
+// // server.use(router1);
+// server.listen(port);
