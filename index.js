e65113708cef8d6f104883156c71f1518c1c1597
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-09-12 23:55:04 +0530

    trip

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 // const jsonServer = require("json-server");
 // const server = jsonServer.create();
 // const middlewares = jsonServer.defaults();
-
-
-
 // const port = process.env.PORT || 8080;
 // server.use(middlewares);
 
@@ -18,6 +15,10 @@
 // server.use("/hrAndCompany", router3);
 
 // server.listen(port);
+//how many data or object you want to show in page
+//https://hr-and-company.onrender.com/hrAndCompany?_page=1&_limit=2
+//for search
+//https://hr-and-company.onrender.com/hrAndCompany?q=emp124
 
 
 const jsonServer = require("json-server"); // importing json-server library
