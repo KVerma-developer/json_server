b5ac8779e5fbdcff1eb03a51ce33c45ac0b4836d
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-09-13 12:26:20 +0530

    trip

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8080;
-// server.use(middlewares);
-
-// // JSON Server Routers
-// const router1 = jsonServer.router("companyDetails.json");
-// const router2 = jsonServer.router("otherData.json");
-// const router3 = jsonServer.router("hr&CompanyContainer.json");
-
-// server.use("/api", router1);
-// server.use("/api2", router2);
-// server.use("/hrAndCompany", router3);
-
 // server.listen(port);
 //how many data or object you want to show in page
 //https://hr-and-company.onrender.com/hrAndCompany?_page=1&_limit=2
@@ -26,7 +10,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("hr&CompanyContainer.json");
 // const router1= jsonServer.router("companyDetails.json")
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; //chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
